67209715f527c10b8237639a4e6710c02dea8d7b
Benjamin Buhler <github@freiken-douhl.de>
2017-05-27 11:55:09 +0200

    Added lowerCaseAttributeNames: false to cheerio options

--- index.js
+++ index.js
@@ -127,7 +127,7 @@ module.exports = function (source, map) {
             );
         }
 
-        var $ = cheerio.load(source, { decodeEntities: false });
+        var $ = cheerio.load(source, { decodeEntities: false, lowerCaseAttributeNames: false });
         var processor = postcss(plugins);
         var promises = [];
 
